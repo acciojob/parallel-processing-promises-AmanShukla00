d43741f5b80fd58522266a5f67f036635eb0644f
AmanShukla00 <aman17101998@gmail.com>
2025-03-06 10:32:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 
@@ -6,7 +7,6 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
-
 function downloadImage(url) {
     return new Promise((resolve, reject) => {
         const img = new Image();
@@ -15,7 +15,6 @@ function downloadImage(url) {
         img.onerror = () => reject(`Failed to load image: ${url}`);
     });
 }
-
 function downloadImages() {
     const loadingDiv = document.getElementById('loading');
     const errorDiv = document.getElementById('error');
